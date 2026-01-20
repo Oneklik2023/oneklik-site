1ccf0b3234c864da15ad7109bf9f6aa9577fbd52
Oneklik2023 <tiwanowski1982@gmail.com>
2026-01-20 18:00:54 +0100

    Refactor HomePage component to use Page component and remove translation logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,5 @@
 "use client";
 
-<<<<<<< HEAD
-import HomeView from "../components/Home/Home";
-import { useTranslation } from "react-i18next";
-
-export default function HomePage() {
-  const { t } = useTranslation();
-
-  return <HomeView />;
-}
-=======
 import Home from '../components/Home/Home';
 
 export default function Page() {
@@ -20,4 +10,3 @@ export default function Page() {
   );
 }
 
->>>>>>> 1714e24a663bcbfe2d737bf380d40a7f428887be

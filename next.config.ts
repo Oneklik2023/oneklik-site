5850c0084b690486d61a4e38da9583bd37203e9c
Oneklik2023 <tiwanowski1982@gmail.com>
2026-01-24 11:56:23 +0100

    20.01.26_3

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
-// next.config.ts
 import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "cms.oneklik.pl" },
-      { protocol: "http",  hostname: "cms.oneklik.pl" }, // jeśli gdzieś trafia http
+      { protocol: "https", hostname: "cms.oneklik.pl", pathname: "/**" },
+      { protocol: "http", hostname: "cms.oneklik.pl", pathname: "/**" }, // tymczasowo, jeśli URL jest http
     ],
-    deviceSizes: [640, 768],
-    imageSizes: [100, 150, 200],
-    minimumCacheTTL: 60 * 60 * 24,
   },
 };
+
 export default nextConfig;
